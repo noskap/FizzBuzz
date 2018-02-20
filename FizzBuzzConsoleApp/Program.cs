10aed3e8090f4cc26e87049d64a2517f25047981
Tom <otacon@live.com.au>
2018-02-20 22:29:37 +1000

    Expanded and extracted fb and console methods

--- FizzBuzzConsoleApp/Program.cs
+++ FizzBuzzConsoleApp/Program.cs
@@ -6,33 +6,58 @@ using System.Threading.Tasks;
 
 namespace FizzBuzzConsoleApp
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Fizz Buzz counter");
-            Console.Write("How high to count?");
-            Console.WriteLine(fB(int.Parse(Console.ReadLine())));
+            StartFizzBuzz();
+            
 
             Console.ReadLine();
         }
+
+        private static void StartFizzBuzz()
+        {
+            Console.WriteLine("Fizz Buzz counter");
+            Console.Write("How high to count?");
+            int d = 0;
+            if (int.TryParse(Console.ReadLine(), out d))
+            {
+                for (int i = 0; i < d; i++)
+                {
+                    Console.WriteLine(fB(d));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input was not a number.");
+            }
+
+            Console.WriteLine("Continue? y/n");
+            if (Console.ReadLine() == "Y" || Console.ReadLine() == "y")
+            {// lowercase y doesnt work 
+                StartFizzBuzz();
+            }
+        }
+
         public static string fB(int inp)
         {
             string d = "";
             try
             {
                 int i = inp / 3;
-                d += "Fizz";
+                d += "Fizz ";
                 try
                 {
                     i = inp / 5;
                     d += "Buzz";
-                }catch(Exception e)
+                }
+                catch (Exception e)
                 {
                     return "";
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
                 return inp.ToString();
             }
---
+++ FizzBuzzConsoleAppTests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConsoleApp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void fBTest()
+        {
+            Assert.AreEqual("Fizz", Program.fB(3));
+        }
+        [TestMethod()]
+        public void fBTest1()
+        {
+            Assert.AreEqual("Buzz", Program.fB(5));
+        }
+    }
+}
